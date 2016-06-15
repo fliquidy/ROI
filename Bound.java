3d4f74e487a3ef7f7e4830a8fc851e678818d3ff
Fliquidy <kaiyufeng@outlook.com>
2016-06-15 15:23:32 +0800

    max

--- Bound.java
+++ Bound.java
@@ -13,8 +13,10 @@ public class Bound {
         setHotUB(0);
     }
     public double upperBound(){
-        double coldUB = _currentValue + _currentValue > _pastValue?_currentValue:_pastValue;
-        return coldUB > _upperbound?_upperbound:coldUB;
+        //double coldUB = _currentValue + _currentValue > _pastValue?_currentValue:_pastValue;
+        //return coldUB > _upperbound?_upperbound:coldUB;
+        double coldUB = _currentValue * 2;
+        return coldUB;
     }
     public boolean isExact(){
         return _exact;
